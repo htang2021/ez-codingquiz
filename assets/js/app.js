428591f8435dee97404be2146c5d331497fc8bf8
Hung Tang <hungtang@hotmail.com>
2021-02-20 21:10:35 -0800

    brainstorming js functions need

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+// Create a function for countdown clock
+
+
+// Create a function to store list of questions & answers or objects
+
+
+// create a function to check the answers
+
+
+// create a function to cycle through the list of questions/objects
+
+
+// create a function to keep track of a score
+
+
+// create a function to allow user to save score
+
+
+// create a function to store scores 
+
+
+// create a function to retrieve scores
+
+
