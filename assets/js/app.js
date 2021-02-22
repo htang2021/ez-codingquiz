78b84cdee69d72b99559eb29b20efe9e2eaedd80
Hung Tang <hungtang@hotmail.com>
2021-02-22 01:54:19 -0800

    added loops to cycle thru questions and answer choices

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,7 +13,7 @@ var timeLeft = setInterval(function() {
 
 // Create a function to store list of questions & answers or objects
 
-var myObj = {
+var codingQuiz = {
     questions: [
         "winning number?", 
         "last number?", 
@@ -29,54 +29,81 @@ var myObj = {
     answers: [1, 6, 12, 15]
 };
 
+//function to cycle through the list of questions *************
+
+var startQuiz = function() {
+    var numberOfQuestions = codingQuiz.questions.length;
+    //var answersChoicesEl = document.querySelector("#answers-list");
+    for (let i=0; i < numberOfQuestions; i++) {
+        document.getElementById("quiz-question").innerHTML = codingQuiz.questions[i];
+        var numAnswerChoices = codingQuiz.answerChoices[i].length;
+
+        var listChoice = document.getElementById("list-choice");
+        for (let j=0; j < numAnswerChoices; j++){
+            console.log("i is " + i + " and j is " + j);
+            var addButton = document.createElement("LI");
+            addButton.innerHTML = codingQuiz.answerChoices[i][j];
+            document.getElementById("answers-list").appendChild(addButton);
+            //buttons.innerHTML = codingQuiz.answerChoices[i][j];
+
+        }
+    }
+}
+
+startQuiz();
 // create a function to check the answers
 
 
 // create a function to cycle through the list of questions/objects
-var cyclingQuestions = function () {
-    // submit button is clicked, go to the next question
-    var clicked = true;
-    while (clicked) {
-        console.log("Next!");
-    }
-    return "Completed";
-}
 
-// create a function to keep track of a score
+// var cyclingQuestions = function () {
+//     // submit button is clicked, go to the next question
+//     var clicked = true;
+//     while (clicked) {
+//         console.log("Next!");
+//     }
+//     return "Completed";
+// }
 
-var scores = [];
-var userInitials = [];
+// an object that stores user initials & scores
 
-var storeScores = function() {
-    var userScore = 83;
-    var initial = "HT";
-    var goOn = 1;
-    if (goOn === 1) {
-        scores.push(userScore);
-        userInitials.push(initial);
-    }
-}
+// var storeScoresObject = {
+//     userInitials: [],
+//     scores: []
+// }
+
+//function to store user initial & score into the object array
+
+// var storeScores = function() {
+//     var userScore = 83;
+//     var initial = "HT";
+//     var goOn = 1;
+//     if (goOn === 1) {
+//         storeScoresObject.scores.push(userScore);
+//         storeScoresObject.userInitials.push(initial);
+//     }
+// }
 
 // create a function that saves scores and user initials to localStorage
 
+//localStorage.setItem(storeScoresObject);
+
+
 //write to storage
 //values stored in localStorage are of type string, thus need to convert to string
-var obj = {name: "Hung", age: 50, city: "Chicago"};
-var myJSON = JSON.stringify(obj);
-document.getElementById("#place-holder").innerHTML = myJSON;
 
-localStorage.setItem(scores);
-localStorage.setItem(userInitials);
+// var obj = {name: "Hung", age: 50, city: "Chicago"};
+// var myJSON = JSON.stringify(obj);
+// document.getElementById("#place-holder").innerHTML = myJSON;
 
-//reads from storage
-localStorage.getItem(scores);
-localStorage.getItem(userInitials);
+// localStorage.setItem(scores);
+// localStorage.setItem(userInitials);
 
 
 
 // create a function to allow user to save score
 
-var submitToSaveScore = document.getElementById("submit-score");
+//var submitToSaveScore = document.getElementById("submit-score");
 
 
 
@@ -84,5 +111,7 @@ var submitToSaveScore = document.getElementById("submit-score");
 
 
 // create a function to retrieve scores
+//reads from storage
 
+//localStorage.getItem(storeScoresObject);
 
