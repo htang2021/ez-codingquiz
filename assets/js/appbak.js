d4e9ced6e4e82a397abb60dbe041477ed1d73f1c
Hung Tang <hungtang@hotmail.com>
2021-02-28 22:51:12 -0800

    cleaned up the code for readability

--- assets/js/appbak.js
+++ assets/js/appbak.js
@@ -28,13 +28,14 @@ var codingQuiz = [
     }
 ];
 
-// Assiging elements to global variables **************************
+//Assiging elements to global variables **************
+
 const introduction = "Try to answer the following code-related questions " +
     "within the time limit. Keep in mind that incorrect answers will " +
     "penalize your score/time by ten seconds!";
 var headerSection = document.querySelector(".top-row");
 var introTitle = document.querySelector(".introTitle");
-//var intro = document.getElementById("choiceContainer");
+var intro = document.querySelector(".intro");
 var writeUp = document.getElementById("intro-writeup");
 var choiceContainer = document.getElementById("choiceContainer");
 var quizQuestion = document.getElementById("quiz-question");
@@ -42,23 +43,24 @@ var initializeTimer = document.getElementById("countdown-timer");
 var startButtonContainer = document.querySelector(".bottom-section");
 var startButton = document.getElementById("start-button");
 var bottomContainer = document.querySelector(".answerContainer");
-//var showResult = document.getElementById("result-response");
-//var answerChoicesContainer =document.querySelector(".answer-choices");
-//var numberOfQuestions = codingQuiz.length;
+var showResult = document.getElementById("result-response");
+var answerChoicesContainer =document.querySelector(".answer-choices");
+var numberOfQuestions = codingQuiz.length;
 var olParentNode = document.getElementById("answers-list");
-//var quizForm = document.getElementById("quiz-form");
+var quizForm = document.getElementById("quiz-form");
 var currentQuestion = 0;
 var timeAllowed = 0;
-let scoreBoard=[];
-let scoreIndex = 0;
+var timeLeft;
+let score = 0;
 
-// Check if countdown timer is still > 0 *******************
+
+//Check if countdown timer is still > 0
 var stillTimeLeft = function(timeAllowed) {
     timeAllowed > 0? true : false;
 }
 
-// Landing Page to start/re-start Coding Quiz Challenge **************
-const codingQuizChallenge = function() {
+//Landing Page to start/re-start Coding Quiz Challenge **************
+var landingPage = function() {
     document.querySelector(".top-section").style.width = "60%";
     document.querySelector(".intro").style.width = "60%";
     document.querySelector(".bottom-section").style.width = "60%";
@@ -67,15 +69,14 @@ const codingQuizChallenge = function() {
     writeUp.textContent = introduction;
     initializeTimer.textContent = "Time: 0";
     startButtonContainer.innerHTML = "<button id='start-button'>Start Quiz</button>";
-    startingQuiz ();
 };
 
-// Check if answer selected is correct ************************
 var checkAnswer = () => {
 
     let numOfChoices = codingQuiz[currentQuestion].choices.length;
     if (stillTimeLeft){
         for (let j=0; j<numOfChoices; j++) {
+
             //map j number of <button> elements with j index id attribute
             //and listen for click events for each of the <button> elements
             //through j loop
@@ -86,7 +87,7 @@ var checkAnswer = () => {
                     bottomContainer.innerHTML = "<p id='result-response'>Correcto!</p>";
                 } else {
                     bottomContainer.innerHTML = "<p id='result-response'>Wrong!</p>";
-                    timeAllowed -= 10;
+                    timeAllowed -= 60;
                 }
                 currentQuestion++;
                 if (currentQuestion < codingQuiz.length) {
@@ -94,7 +95,8 @@ var checkAnswer = () => {
                     nextQuestion(currentQuestion);
                 } else {
                     quizCompleted();
-                }  
+                }
+                    
             }); 
         }
     } else {
@@ -103,11 +105,9 @@ var checkAnswer = () => {
     }
 }
 
-// Cycle through list of questions in the ojbect array ******************
 var nextQuestion = (currentQuestion) => {
     quizQuestion.textContent = codingQuiz[currentQuestion].question;
     let numOfChoices = codingQuiz[currentQuestion].choices.length;
-    // introTitle.removeAttribute("style");
     if (stillTimeLeft) {
         for (let j=0; j<numOfChoices; j++) {
             //dynamically create elements for each of the answer choices
@@ -128,7 +128,7 @@ var nextQuestion = (currentQuestion) => {
     }
 }
 
-// remove list of answer choices once answer is chosen *******************
+// remove list of answer choices once answer is chosen
 var removeAnswerChoices = (currentQuestion) => {
     let numOfChoices = codingQuiz[currentQuestion].choices.length;
     console.log(`current question is ${currentQuestion} and the length is ${numOfChoices}`);
@@ -139,126 +139,173 @@ var removeAnswerChoices = (currentQuestion) => {
     }
 }
 
-// Click which option, to restart or to clear score *****************
-var goBackOrClear = function(goBackButton, clearScore) {
+var quizCompleted = () => {
+    clearInterval(timeLeft);
+    score = timeLeft; 
+    quizQuestion.textContent = "";
+    choiceContainer.textContent = "";
+    bottomContainer.textContent = "";
+    quizQuestion.textContent = "All Done!";
+    choiceContainer.innerHTML = `<p>Your final score is ${timeAllowed}.
+    <br>
+    <form id="submit-form">Enter Initials: 
+    <input type="text" id="txtInitials" name="txtInitials">
+    <input type="button" id="btnSubmit" value="Submit">
+    </form>`;
 
-    goBackButton.addEventListener("click", function() {
-        choiceContainer.textContent = ""
-        // bottomContainer.textContent = "";
-        // quizQuestion.textContent = "";
-        headerSection.style.visibility = "visible";
-        choiceContainer.innerHTML = "<p id='intro-writeup'></p>";
-        codingQuizChallenge();
-    });
+    // var userInitialsInputForm = document.createElement("form");
+    // userInitialsInputForm.id = "submit-form";
 
-    clearScore.addEventListener("click", function() {
-        quizQuestion.textContent = "";
-        choiceContainer.textContent = "";
-        bottomContainer.textContent = "";
-        quizQuestion.textContent = "High Scores";
-        choiceContainer.innerHTML = `<li>${userInitials}</li>`;
-    });
-}
+    // var userInitialInputField = document.createElement("input");
+    // userInitialInputField.type = "text";
+    // userInitialInputField.id = "initials";
+    // userInitialInputField.name = "initials";
 
-// Write userInitials & scores to array *************************
-var writeUserInitialsAndScoreToArray = (userInitials, score) => {
-    var scoreBoard = [];
-    scoreBoard.push({'storedInitial': userInitials, 'storedScore': score});
-}
+    // var userInitialsSubmitButton = document.createElement("button");
+    // userInitialsSubmitButton.type = "submit";
+    // userInitialsSubmitButton.value = "Submit";
 
-// Initials and score handling *******************************
-var initialAndScoreSubmissionHandler = (txtInitials) => {
+    // userInitialsInputField.appendChild(userInitialsSubmitButton);
+    // userInitialsInputForm.appendChild(userInitialInputField);
+
+    //submitInitialsAndScore();
+    var initialSubmit = document.getElementById("submit-form");
+    var btnSubmit = document.getElementById("btnSubmit");
+    var txtInitials = document.getElementById("txtInitials");
+    //txtInitials.value = "HT_AutoAssign";
+    //var userInitials = document.querySelector("input[name='initials']").value;
+    //var userInitials = document.querySelector("#initials").value;
+    btnSubmit.addEventListener("click", (function(txtInitials) {
         headerSection.style.visibility = "hidden";
         quizQuestion.textContent = "";
         choiceContainer.textContent = "";
         bottomContainer.textContent = "";
 
         quizQuestion.textContent = "High Scores";
+        //var userInitials = document.querySelector("input[name='initials']").value;
+        var localtxtInitials = document.getElementById("txtInitials");
         var userInitials = txtInitials.value;
         choiceContainer.innerHTML = `<li>${userInitials} - ${timeAllowed+1}</li><br>
             <button type="button" value="goBack" id="goBack">Go Back</button>
             <button type="button" value="clearScore" id="clearScore">Clear High Scores</button>`;
-        
-        // Call a function to write userInitials and timeAllowed to an Array
-        writeUserInitialsAndScoreToArray(userInitials, timeAllowed+1);
-
+    })(txtInitials));
+    //storeScore();
+    window.onload = function() {
         var goBackButton = document.getElementById("goBack");
         var clearScore = document.getElementById("clearScore");
-
-        goBackOrClear(goBackButton, clearScore);
-}
-
-// Quiz completed or stopped ***************************
-var quizCompleted = () => {
-    countDownClock(0);
-    quizQuestion.textContent = "";
-    //choiceContainer.textContent = "";
-    writeUp.textContent = "";
-    bottomContainer.textContent = "";
-    quizQuestion.textContent = "All Done!";
-    choiceContainer.style.fontWeight = "bold";
-    choiceContainer.innerHTML = `<p>Your final score is ${timeAllowed}.
-    <br><br>
-    <label for="txtInitials">Enter Initials: </label>
-    <input type="text" id="txtInitials" name="txtInitials">
-    <button type="button" id="btnSubmit" value="button">Submit</button>`;
- 
-    var btnSubmit = document.getElementById("btnSubmit");
-    var txtInitials = document.getElementById("txtInitials");
-
-    btnSubmit.addEventListener("click", function() {
-        initialAndScoreSubmissionHandler(txtInitials);
-    });
-}
-
-// Counting down from 75 seconds **************************
-var countDownClock = function(goOrStop) {
-    if (goOrStop === 1) {  //1 for go and anything else for stop
-        timeAllowed = 75;
-        var countDown = setInterval(startCounting, 1000);
-
-        function startCounting () {
-            if (timeAllowed < 0) {
-                clearInterval(countDown);
-                timeAllowed = 0;
-                quizCompleted();
-            } else {
-                document.getElementById("timer").innerHTML = "Time: " + timeAllowed;
-            }
-            timeAllowed -=1;
-            stillTimeLeft(timeAllowed);
-        }
-    } else {
-        clearInterval(countDown);
+        goBackButton.addEventListener("click", landingPage);// {
+            // quizQuestion.textContent = "";
+            // choiceContainer.textContent = "";
+            // bottomContainer.textContent = "";
+        //     landingPage();
+        // });
+        clearScore.addEventListener("click", function() {
+            quizQuestion.textContent = "";
+            choiceContainer.textContent = "";
+            bottomContainer.textContent = "";
+            quizQuestion.textContent = "High Scores";
+        choiceContainer.innerHTML = `<li>${userInitials}</li>`;
+        });
     }
 }
 
-// Quiz has begun! *****************************************
+// var storeScore = function() {
+//     var goBackButton = document.getElementById("goBackButton");
+//     var clearScore = document.getElementById("clearScore");
+//     goBackButton.addEventListener("click", landingPage);// {
+//         // quizQuestion.textContent = "";
+//         // choiceContainer.textContent = "";
+//         // bottomContainer.textContent = "";
+//     //     landingPage();
+//     // });
+//     clearScore.addEventListener("click", function() {
+//         quizQuestion.textContent = "";
+//         choiceContainer.textContent = "";
+//         bottomContainer.textContent = "";
+//         quizQuestion.textContent = "High Scores";
+//     choiceContainer.innerHTML = `<li>${userInitials}</li>`;
+//     });
+// }
+
+
+// var storeScore = function() {
+//     quizQuestion.textContent = "";
+//     choiceContainer.textContent = "";
+//     bottomContainer.textContent = "";
+//     quizQuestion.textContent = "High Scores";
+//     choiceContainer.innerHTML = "<li>userInitials</li>"
+// }
+
+// //Check if countdown timer is still > 0
+// var stillTimeLeft = function(timeAllowed) {
+//     timeAllowed > 0? true : false;
+// }
+
+//Counting down from 75 seconds
+var countDownClock = function() {
+    timeAllowed = 3000;
+    var timeLeft = setInterval(function() {
+        if (timeAllowed < 0) {
+            clearInterval(timeLeft);
+            timeAllowed = 0;
+            quizCompleted();
+            //clearInterval(timeLeft);
+        } else {
+            document.getElementById("timer").innerHTML = "Time: " + timeAllowed;
+        }
+        timeAllowed -=1;
+        stillTimeLeft(timeAllowed);
+    }, 1000);
+}
+
+//Quiz has begun! *****************************************
+
 function quizStarted() {
     //clear the content in the container
-    //intro.removeChild(writeUp);
-    writeUp.textContent = '';
+    intro.removeChild(writeUp);
     startButtonContainer.innerHTML='';
-    currentQuestion = 0;
     //Timer starts ***
-    countDownClock(1); //passing 1 to start countdown
+    countDownClock();
     if (stillTimeLeft) {
-        introTitle.removeAttribute("style");
         nextQuestion(currentQuestion);
     } else {
          console.log("Time has run out");
     };
 }
 
-//Click the button to start the quiz!!  **********************
+
+
+//Click the button to start the quiz!!  ****************
 var startingQuiz = function() {
     var startButton = document.getElementById("start-button");
     startButton.addEventListener("click", quizStarted);
 
 }
 
-codingQuizChallenge();
+landingPage();
+startingQuiz ();
 
+
+
+
+// an object that stores user initials & scores
+
+// var storeScoresObject = {
+//     userInitials: [],
+//     scores: []
+// }
+
+//function to store user initial & score into the object array
+
+// var storeScores = function() {
+//     var userScore = 83;
+//     var initial = "HT";
+//     var goOn = 1;
+//     if (goOn === 1) {
+//         storeScoresObject.scores.push(userScore);
+//         storeScoresObject.userInitials.push(initial);
+//     }
+// }
 
 // create a function that saves scores and user initials to localStorage
 
@@ -275,6 +322,15 @@ codingQuizChallenge();
 // localStorage.setItem(scores);
 // localStorage.setItem(userInitials);
 
+
+
+// create a function to allow user to save score
+
+//var submitToSaveScore = document.getElementById("submit-score");
+
+
+
+// create a function to store scores 
 
 
 // create a function to retrieve scores
